f705889021dd0164ec03a434e7749204e68ad6a7
Raphael Karanja <rkaranja@cytonn.com>
2018-12-05 18:51:06 +0300

    add page routes and wrap everything under BrowserRouter and child Fragment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 export default class extends Component {
     state = {
@@ -6,15 +7,28 @@ export default class extends Component {
     };
 
     async componentDidMount() {
-       const authors = await (await fetch('http://localhost:3004/authors') ).json();
+        const authors = await (await fetch('http://localhost:3004/authors') ).json();
         this.setState({authors});
     };
 
     render() {
         return (
-            <div className="App">
+            <BrowserRouter>
+                <Fragment className="App">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/authors">Authors</Link>
+                        </li>
+                        <li>
+                            <Link to="/articles">Articles</Link>
+                        </li>
+                    </ul>
+                </Fragment>
+            </BrowserRouter>
 
-            </div>
         );
     }
 };
