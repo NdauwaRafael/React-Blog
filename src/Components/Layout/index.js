8f07a371fcd82bbd2810c6b7abcc75bf0ad93e6f
Raphael Karanja <rkaranja@cytonn.com>
2018-12-06 15:59:51 +0300

    make side bar clipped under top bar

--- src/Components/Layout/index.js
+++ src/Components/Layout/index.js
@@ -1,7 +1,7 @@
 /**
  * Created by Raphael Karanja on 06/12/2018.
  */
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {
     AppBar,
     CssBaseline,
@@ -20,7 +20,6 @@ import MailIcon from '@material-ui/icons/Mail';
 import MenuIcon from '@material-ui/icons/Menu';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import {withStyles} from '@material-ui/core/styles';
-
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -34,10 +33,7 @@ const styles = theme => ({
         },
     },
     appBar: {
-        marginLeft: drawerWidth,
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-        },
+        zIndex: theme.zIndex.drawer + 1,
     },
     menuButton: {
         marginRight: 20,
@@ -69,6 +65,7 @@ class Layout extends Component {
         const {mobileOpen} = this.state;
 
         const drawer = (
+
             <div>
                 <div className={classes.toolbar}/>
                 <Divider/>
@@ -92,60 +89,63 @@ class Layout extends Component {
             </div>
         );
         return (
-            <div className={classes.root}>
-                <CssBaseline/>
-                <AppBar position="fixed" className={classes.appBar}>
-                    <Toolbar>
-                        <IconButton
-                            color="inherit"
-                            aria-label="Open drawer"
-                            onClick={this.handleDrawerToggle}
-                            className={classes.menuButton}
-                        >
-                            <MenuIcon/>
-                        </IconButton>
-                        <Typography variant="h6" color="inherit" noWrap>
-                            Responsive drawer
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <nav className={classes.drawer}>
-                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                    <Hidden smUp implementation="css">
-                        <Drawer
-                            container={this.props.container}
-                            variant="temporary"
-                            open={mobileOpen}
-                            onClose={this.handleDrawerToggle}
-                            classes={{
-                                paper: classes.drawerPaper,
-                            }}
-                            ModalProps={{
-                                keepMounted: true, // Better open performance on mobile.
-                            }}
-                        >
-                            {drawer}
-                        </Drawer>
-                    </Hidden>
-                    <Hidden xsDown implementation="css">
-                        <Drawer
-                            classes={{
-                                paper: classes.drawerPaper,
-                            }}
-                            variant="permanent"
-                            open
-                        >
-                            {drawer}
-                        </Drawer>
-                    </Hidden>
-                </nav>
-                <main className={classes.content}>
-                    <div className={classes.toolbar}/>
-                    {children}
-                </main>
-            </div>
+            <Fragment>
+                <CssBaseline />
+                <div className={classes.root}>
+                    <CssBaseline/>
+                    <AppBar position="fixed" className={classes.appBar}>
+                        <Toolbar>
+                            <IconButton
+                                color="inherit"
+                                aria-label="Open drawer"
+                                onClick={this.handleDrawerToggle}
+                                className={classes.menuButton}
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                            <Typography variant="h6" color="inherit" noWrap>
+                                Responsive drawer
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <nav className={classes.drawer}>
+                        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                        <Hidden smUp implementation="css">
+                            <Drawer
+                                container={this.props.container}
+                                variant="temporary"
+                                open={mobileOpen}
+                                onClose={this.handleDrawerToggle}
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                                ModalProps={{
+                                    keepMounted: true, // Better open performance on mobile.
+                                }}
+                            >
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                        <Hidden xsDown implementation="css">
+                            <Drawer
+                                classes={{
+                                    paper: classes.drawerPaper,
+                                }}
+                                variant="permanent"
+                                open
+                            >
+                                {drawer}
+                            </Drawer>
+                        </Hidden>
+                    </nav>
+                    <main className={classes.content}>
+                        <div className={classes.toolbar}/>
+                        {children}
+                    </main>
+                </div>
+            </Fragment>
         );
     }
 };
 
-export default withStyles(styles, { withTheme: true })(Layout);
+export default withStyles(styles, {withTheme: true})(Layout);
