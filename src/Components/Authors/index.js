7258142ef21629518db2770f194f6d772753e2fb
Raphael Karanja <rkaranja@cytonn.com>
2018-12-06 10:00:55 +0300

    add a redirect component whenver one tries to access an none existing author id

--- src/Components/Authors/index.js
+++ src/Components/Authors/index.js
@@ -2,7 +2,7 @@
  * Created by Raphael Karanja on 05/12/2018.
  */
 import React, {Fragment} from 'react';
-import {Route, Link, Switch} from 'react-router-dom';
+import {Route, Link, Redirect} from 'react-router-dom';
 import Author from './Author/index';
 
 
@@ -19,11 +19,16 @@ export default ({match: {url}, authors}) =>
                 </ul>
             </div>
             <div className="col-9">
-                <Switch>
-                    <Route exact  path={url} render={()=> <div><h3>Please select an Author from the above:</h3></div>} />
-                    <Route path={`${url}/:authorId`} render={({match})=> <Author  {...authors.find(author => author.id === parseInt(match.params.authorId))}/>}/>
-                    <Route render={()=><div><h3>Not found</h3></div>}/>
-                </Switch>
+                <Route exact  path={url} render={()=> <div><h3>Please select an Author from the above:</h3></div>} />
+                <Route path={`${url}/:authorId`} render={({match})=> {
+                    const author = authors.find(author => author.id === parseInt(match.params.authorId));
+                    if(!author){
+                        return <Redirect to="/404"/>
+                    }else {
+                      return  <Author  {...author}/>
+                    }
+                }
+                }/>
             </div>
         </div>
 
