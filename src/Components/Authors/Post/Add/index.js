c09663520ca0f3ad8d9e4b8eeb533df5cdf5ff8d
Raphael Karanja <rkaranja@cytonn.com>
2018-12-07 11:20:58 +0300

    style add post component

--- src/Components/Authors/Post/Add/index.js
+++ src/Components/Authors/Post/Add/index.js
@@ -4,8 +4,11 @@
 import React, {Component} from 'react';
 import {
     TextField,
+    Button,
+    Icon
 } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
+import classNames from 'classnames';
 
 const styles = theme => ({
     container: {
@@ -14,14 +17,22 @@ const styles = theme => ({
     },
     textField: {
         marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        width: 200,
+        marginRight: theme.spacing.unit
     },
     dense: {
         marginTop: 19,
     },
-    menu: {
-        width: 200,
+    button: {
+        margin: theme.spacing.unit
+    },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+    iconSmall: {
+        fontSize: 20,
     },
 });
 
@@ -36,39 +47,49 @@ class AddPost extends Component {
             [name]: event.target.value,
         });
     };
+
     render() {
         const {classes} = this.props;
         return (
-            <div>
-                <form className={classes.container} noValidate autoComplete="off">
-                    <TextField
-                        id="standard-full-width"
-                        label="Label"
-                        style={{margin: 8}}
-                        placeholder="Placeholder"
-                        helperText="Post Title"
-                        fullWidth
-                        margin="normal"
-                        value={this.state.title}
-                        onChange={this.handleChange('title')}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
+            <div style={{marginTop: 30}}>
+                <div className="card">
+                    <h5 className="card-header">Add Post</h5>
+                    <div className="card-body">
+                        <form className={classes.container} noValidate autoComplete="off">
+                            <TextField
+                                id="standard-full-width"
+                                label="Post Title"
+                                style={{margin: 8}}
+                                placeholder="Post Title"
+                                fullWidth
+                                margin="normal"
+                                value={this.state.title}
+                                onChange={this.handleChange('title')}
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                            />
 
-                    <TextField
-                        id="standard-textarea"
-                        label="With placeholder multiline"
-                        placeholder="Post Description"
-                        multiline
-                        fullWidth
-                        value={this.state.body}
-                        onChange={this.handleChange('body')}
-                        className={classes.textField}
-                        margin="normal"
-                    />
+                            <TextField
+                                id="standard-textarea"
+                                label="Post Description"
+                                placeholder="Post Description"
+                                multiline
+                                rows="4"
+                                fullWidth
+                                value={this.state.body}
+                                onChange={this.handleChange('body')}
+                                className={classes.textField}
+                                margin="normal"
+                            />
+                        </form>
+                        <Button variant="outlined" color="primary" className={classes.button}>
+                            Save
+                            <Icon className={classNames(classes.rightIcon, classes.iconSmall)}>send</Icon>
+                        </Button>
+                    </div>
+                </div>
 
-                </form>
             </div>
         )
     }
