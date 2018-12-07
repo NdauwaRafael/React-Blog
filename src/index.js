f55ffe66f97bee96f077f07d4cfc175e2a5b54d3
Raphael Karanja <rkaranja@cytonn.com>
2018-12-07 12:18:32 +0300

    remove lowd files import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import $ from 'jquery';
 // import Popper from 'popper.js';
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 // import 'material-design-icons'
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import App from './Components/App';
-import FileSync from 'lowdb/adapters/FileSync';
 
-const low = require('lowdb');
+// const low = require('lowdb');
 // const adapter = new FileSync('listio.json');
 // const db = low(adapter);
 
